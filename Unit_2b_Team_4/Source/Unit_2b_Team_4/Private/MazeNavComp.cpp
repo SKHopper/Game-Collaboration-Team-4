ae081e714c6fef934d2f7bc623b1439b1ef3adb2
SKHopper <skojo.hopper@yahoo.com>
2026-03-09 15:31:35 +0000

    Sam H | Compileable maze navigation c++ component

---
+++ Unit_2b_Team_4/Source/Unit_2b_Team_4/Private/MazeNavComp.cpp
@@ -0,0 +1,144 @@
+
+
+
+#include "MazeNavComp.h"
+
+
+// Sets default values for this component's properties
+UMazeNavComp::UMazeNavComp()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMazeNavComp::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+TArray<FIntVector2>  UMazeNavComp::pathfind(FIntVector2& start, FIntVector2& target, float& searchDistance) {
+
+	if (FVector2D::Distance({ double(start.X), double(start.Y) }, { double(target.X), double(target.Y) }) > searchDistance) {
+		return {}; 
+	}
+
+	TMap<FIntVector2, FNavNode> toSearch;
+	TSet<FIntVector2> searched;
+	TMap<FIntVector2, FIntVector2> bestPrevious;
+
+	FNavNode startNode;
+	startNode.pos = start;
+	startNode.gCost = 0.f;
+	startNode.hCost = heuristic(start, target);
+	toSearch.Add(start, startNode);
+
+	while (toSearch.Num() > 0) {
+
+		//get lowest
+
+		FIntVector2 current = { 0, 0 };
+		float lowestFCost = 1234567890;
+		
+		for (TPair<FIntVector2, FNavNode>& searchPair : toSearch) {
+			if (searchPair.Value.fCost() < lowestFCost) {
+				lowestFCost = searchPair.Value.fCost();
+				current = searchPair.Key;
+			}
+		}
+
+		//check there yet
+		if (current == target) { return makePath(bestPrevious, current); }
+
+		//not there yet move on
+		FNavNode currentNode = toSearch[current];
+		toSearch.Remove(current);
+		searched.Add(current);
+
+		for (FPositionDistance neighbourPair : neighboursWithDistance(current)) {
+			FIntVector2 neighbour = neighbourPair.pos;
+			float distance = neighbourPair.distance;
+
+			//validate
+			if (
+				searched.Contains(neighbour) 
+				or isBlocked(neighbour)
+				or FVector2D::Distance({ double(start.X), double(start.Y) }, { double(neighbour.X), double(neighbour.Y) }) > searchDistance
+			) { 
+				continue; 
+			}
+
+			float newGCost = currentNode.gCost + distance;
+
+			if (not toSearch.Contains(neighbour) or newGCost < toSearch.Find(neighbour)->gCost) {
+
+				FNavNode neighbourNode;
+				neighbourNode.pos = neighbour;
+				neighbourNode.gCost = newGCost;
+				neighbourNode.hCost = heuristic(neighbour, target);
+
+				toSearch.Add(neighbour, neighbourNode);
+				bestPrevious.Add(neighbour, current);
+			}
+		}
+	}
+	
+	//fail
+	return {};
+}
+
+float UMazeNavComp::heuristic(FIntVector2 from, FIntVector2 to)
+{
+	int32 x = FMath::Abs(from.X - to.X);
+	int32 y = FMath::Abs(from.Y - to.Y);
+	return 1.414f * FMath::Min(x, y) + FMath::Abs(x - y);
+}
+
+TArray<FIntVector2> UMazeNavComp::makePath(TMap<FIntVector2, FIntVector2>& previous, FIntVector2& end) {
+	TArray<FIntVector2> path;
+
+	FIntVector2 current = end;
+	//prepend from end
+	while (previous.Contains(current)) {
+		path.Insert(current, 0);
+		current = previous[current];
+	}
+
+	//needs start
+	path.Insert(current, 0);
+	return path;
+	
+}
+
+TArray<FPositionDistance> UMazeNavComp::neighboursWithDistance(FIntVector2& centre)
+{
+	return {
+		//norf souf eass wess
+		{ FIntVector2(centre.X + 1, centre.Y), 1.0f },
+		{ FIntVector2(centre.X - 1, centre.Y), 1.0f },
+		{ FIntVector2(centre.X, centre.Y + 1), 1.0f },
+		{ FIntVector2(centre.X, centre.Y - 1), 1.0f },
+		//diagonal
+		{ FIntVector2(centre.X + 1, centre.Y + 1), sqrtTwo },
+		{ FIntVector2(centre.X - 1, centre.Y + 1), sqrtTwo },
+		{ FIntVector2(centre.X + 1, centre.Y - 1), sqrtTwo },
+		{ FIntVector2(centre.X - 1, centre.Y - 1), sqrtTwo }
+	};
+}
+
+// Called every frame
+void UMazeNavComp::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+
---
+++ Unit_2b_Team_4/Source/Unit_2b_Team_4/Public/MazeNavComp.h
@@ -0,0 +1,77 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MazeNavComp.generated.h"
+
+//shmovin' diagonally
+#define sqrtTwo 1.414f
+
+//https://www.geeksforgeeks.org/dsa/a-search-algorithm/
+
+USTRUCT()
+struct FNavNode {
+	GENERATED_BODY()
+
+	//g = from start   h = heuristic direct
+
+	FIntVector2 pos;
+	float gCost = 0.f;
+	float hCost = 0.f;
+	FIntVector2 parent = FIntVector2(-1, -1);
+
+	float fCost() {
+		return gCost + hCost;
+	}
+};
+
+USTRUCT()
+struct FPositionDistance {
+
+	GENERATED_BODY()
+
+	FIntVector2 pos;
+	float distance = 1.0f;
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UNIT_2B_TEAM_4_API UMazeNavComp : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMazeNavComp();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	//our location
+	UPROPERTY(BlueprintReadWrite)
+	FIntVector2 myPos;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FIntVector2> pathfind(FIntVector2& start, FIntVector2& target, float& searchDistance);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	bool isBlocked(FIntVector2 point);
+
+	UFUNCTION()
+	float heuristic(FIntVector2 from, FIntVector2 to);
+
+	UFUNCTION()
+	TArray<FIntVector2> makePath(TMap<FIntVector2, FIntVector2>& previous, FIntVector2& end);
+
+	UFUNCTION()
+	TArray<FPositionDistance> neighboursWithDistance(FIntVector2& centre);
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	
+};
